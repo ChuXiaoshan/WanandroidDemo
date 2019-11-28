759bd2417dc898639b1f0e99f1c33eab2c21ef62
CxS <xiaoshan1215@gmail.com>
2019-11-28 18:11:08 +0800

    iii

--- app/src/main/java/com/cxsplay/wad/MainFragment.kt
+++ app/src/main/java/com/cxsplay/wad/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.cxsplay.wad
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -13,8 +15,15 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = MainFragment()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        btn.setOnClickListener {
+            Toast.makeText(activity, "ddd", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.blankFragment2)
+        }
     }
 }
